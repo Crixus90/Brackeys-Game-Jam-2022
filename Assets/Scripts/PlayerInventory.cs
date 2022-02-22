3e0f935a12f3ec373a93445da52c1557b968ca06
PSokol <williamsokol0@gmail.com>
2022-02-22 12:58:46 -0800

    added pickup action

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+
+    bool hasPill = false;
+
+    public void PickUpFunc(GameObject obj)
+    {
+        StartCoroutine(PickUp(obj));
+    }
+    public IEnumerator PickUp(GameObject obj)
+    {
+        Vector3 pos = transform.position;
+        pos.y += 1;
+        while(Vector3.Distance(obj.transform.position, pos) > .1)
+        {
+            obj.transform.position = Vector3.Lerp(obj.transform.position, pos, .02f);
+           
+            yield return null;
+        }
+
+        hasPill = true;
+        Destroy(obj);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
