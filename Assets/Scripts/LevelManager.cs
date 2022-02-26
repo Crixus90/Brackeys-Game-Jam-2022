2f99b54d7ef95157ada0daef04d9d3c9ffef1058
PSokol <williamsokol0@gmail.com>
2022-02-26 02:05:25 -0800

    added fade effect and menu buttons work

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,3 +1,5 @@
+// this script is there to handle things that happen in a level
+// 2/25/2022
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +7,29 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject playerObj;
     void Start()
     {
         
     }
 
-    public void LoadLevel()
+    void Update()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if(playerObj != null){
+
+            if(playerObj.transform.position.y < - 5)
+            {
+                print("fallen");
+                LoadLevel(2);
+            }
+        }
+    }
+
+    public void LoadLevel(int scene = -1)
+    {
+        if(scene == -1){
+            scene = SceneManager.GetActiveScene().buildIndex + 1;
+        }
+        SceneManager.LoadScene(scene);
     }
 }
---
+++ Assets/Scripts/Sounds/MusicPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+  
+    //public AudioListener AudioListener;
+    public AudioMixer mixer;
+    public MusicPlayer instance;
+    void Start()
+    {
+        if(instance != null){
+            Destroy(this.gameObject);
+        }else{
+            instance  = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        //AudioListener =  GetComponent<AudioSource>();
+        //AudioListener.Play();
+        //SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public void SetVolume(float sliderValue){
+        mixer.SetFloat("vol",Mathf.Log10 (sliderValue) *20);
+    }
+    
+
+    public IEnumerator FadeMusic ( float fadeTime, float volumeGoal) {
+        float startVolume = AudioListener.volume;
+ 
+        //float velocity = 0f;
+        if(volumeGoal ==0){
+            while (AudioListener.volume > volumeGoal) {
+                AudioListener.volume -= (startVolume * Time.deltaTime / fadeTime);
+                //AudioListener.volume = Mathf.SmoothDamp(AudioListener.volume, volumeGoal, ref velocity, fadeTime);
+
+                yield return null;
+            } 
+        }else{
+            while (AudioListener.volume < volumeGoal-.0001f) {
+                AudioListener.volume += (volumeGoal * Time.deltaTime / fadeTime);
+                //musicSource.volume = Mathf.SmoothDamp(musicSource.volume, volumeGoal, ref velocity, fadeTime);
+
+                yield return null;
+            } 
+        }
+    }
+}
